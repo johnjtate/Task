bf83817b97e23c3a7e767fbe7526ed22c798ca10
John Tate <johntate@Johns-MacBook-Pro.local>
2018-08-29 19:20:23 -0600

    first commit

---
+++ Task/DateHelpers.swift
@@ -0,0 +1,9 @@
+//
+//  DateHelpers.swift
+//  Task
+//
+//  Created by John Tate on 8/29/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+import Foundation
---
+++ Task/Views/ButtonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ButtonTableViewCell.swift
+//  Task
+//
+//  Created by John Tate on 8/29/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+import UIKit
+
+class ButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
